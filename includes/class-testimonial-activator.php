a72d7cba671f9ec5ec47b7614e5f1e95920261fa
Denis <web@dgladkov.ru>
2021-03-15 21:35:59 +0300

    Create empty plugin

---
+++ includes/class-testimonial-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       z-website.ru
+ * @since      1.0.0
+ *
+ * @package    Testimonial
+ * @subpackage Testimonial/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Testimonial
+ * @subpackage Testimonial/includes
+ * @author     Gladkov Denis <info@z-website.ru>
+ */
+class Testimonial_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-testimonial-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       z-website.ru
+ * @since      1.0.0
+ *
+ * @package    Testimonial
+ * @subpackage Testimonial/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Testimonial
+ * @subpackage Testimonial/includes
+ * @author     Gladkov Denis <info@z-website.ru>
+ */
+class Testimonial_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
