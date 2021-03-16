e822e1718f6bc7f66cbaf00381f204fb7905d785
Denis <web@dgladkov.ru>
2021-03-16 12:35:29 +0300

    Proba

--- testimonial_zw/includes/class-testimonial_zw.php
+++ testimonial_zw/includes/class-testimonial_zw.php
@@ -216,3 +216,9 @@ class Testimonial_zw {
 	}
 
 }
+// Add menu item
+   $this->loader->add_action( 'admin_menu', $plugin_admin, 'add_plugin_admin_menu' );
+
+   // Add Settings link to the plugin
+   $plugin_basename = plugin_basename( plugin_dir_path( __DIR__ ) . $this->plugin_name . '.php' );
+   $this->loader->add_filter( 'plugin_action_links_' . $plugin_basename, $plugin_admin, 'add_action_links' );
